15da6d61925b68d52dc70295418596c9b9d11728
shariarshah611-dot <shariarshah611@gmail.com>
2026-02-13 22:13:08 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function order(product, price) {
+  document.getElementById("orderForm").scrollIntoView();
+  document.getElementById("product").value = product;
+  document.getElementById("price").value = price + "৳";
+}
+
+function submitOrder(event) {
+  event.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let product = document.getElementById("product").value;
+  let price = document.getElementById("price").value;
+
+  let message = `New Order%0AName: ${name}%0APhone: ${phone}%0AProduct: ${product}%0APrice: ${price}`;
+
+  window.open(`https://wa.me/8801302133194?text=${message}`, '_blank');
+}
